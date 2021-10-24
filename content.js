9029609a065c1528770afc56850fa366ad0b3631
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-10-24 10:26:57 +0300

    Try some more

--- content.js
+++ content.js
@@ -1,6 +1,22 @@
+document.getElementsByTagName('html')[0].style.overflow = 'auto';
+document.getElementsByTagName('body')[0].style.overflow = 'auto';
 console.log("ALABALA");
-let speedXVid = 2.9;
-let speedXAudio = 2.9;
+//localStorage.setItem('speedXVid', 2.5);
+//localStorage.setItem('speedXAudio', 2.5);
+let speedXVid;
+browser.storage.local.set({'speedXVid', 2.5})
+.then(browser.storage.local.get('speedXVid')
+	.then((item) => {
+		speedXVid = item.speedXVid;
+		console.log(item.speedXVid);
+	})
+);
+	
+//console.log('speed: ', await browser.storage.local.get('speedXVid'));
+//let speedXVid = await browser.storage.local.get('speedXVid');
+//localStorage.getItem('speedXVid') ?? 2.9;
+let speedXAudio = localStorage.getItem('speedXAudio') ?? 2.9;
+
 [...document.getElementsByTagName('video')].forEach(el => el.playbackRate = speedXVid);
 [...document.getElementsByTagName('audio')].forEach(el => el.playbackRate = speedXAudio);
 
--- popup.js
+++ popup.js
@@ -4,7 +4,33 @@ window.browser = (function () {
     window.chrome;
 })();
 
-function getSpeed()
+document.addEventListener('DOMContentLoaded', function() {
+    const speedUp = document.getElementById('speedValue');
+	const on = document.getElementById('buttonON');
+    speedUp.addEventListener('change', function() {
+		console.log(Number(speedUp.value));
+		if(speedUp.value) localStorage.setItem('speedXVid', Number(speedUp.value));
+    });
+	on.addEventListener('change', function() {
+		console.log(Number(speedUp.value));
+		localStorage.setItem('speedXVid', Number(speedUp.value));
+    });
+});
+
+function speedUp(event){
+	event.preventDefault();
+	let speedup = Number(document.getElementById("speedValue").value);
+	console.log(speedup);
+	localStorage.setItem('speedXVid', speedup);
+}
+
+function speedUpDirect(event){
+	event.preventDefault();
+	console.log(Number(event.target.value));
+	localStorage.setItem('speedXVid', Number(event.target.value));
+}
+
+/*function getSpeed()
 {
   speedUp = document.getElementById("speedValue").value;
   console.log(speedUp);
@@ -24,4 +50,4 @@ document.getElementById("buttonON").addEventListener("click", event =>{
   {
   browser.tabs.sendMessage(tabs[0].id, speedUp);
   });
-});
+});*/
