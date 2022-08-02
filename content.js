be287587a1d7875f8ba0835772f1cbf98794bc53
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2022-08-02 17:03:43 +0300

    Add changes

--- content.js
+++ content.js
@@ -1,25 +1,49 @@
-document.getElementsByTagName('html')[0].style.overflow = 'auto';
-document.getElementsByTagName('body')[0].style.overflow = 'auto';
+//document.getElementsByTagName('html')[0].style.overflow = 'auto';
+//document.getElementsByTagName('body')[0].style.overflow = 'auto';
 console.log("ALABALA");
-//localStorage.setItem('speedXVid', 2.5);
-//localStorage.setItem('speedXAudio', 2.5);
-let speedXVid;
+/*let speedXVid;
 browser.storage.local.set({'speedXVid', 2.5})
 .then(browser.storage.local.get('speedXVid')
 	.then((item) => {
 		speedXVid = item.speedXVid;
 		console.log(item.speedXVid);
 	})
-);
+);*/
 	
 //console.log('speed: ', await browser.storage.local.get('speedXVid'));
 //let speedXVid = await browser.storage.local.get('speedXVid');
-//localStorage.getItem('speedXVid') ?? 2.9;
+
+localStorage.setItem('speedXVid', 2.7);
+localStorage.setItem('speedXAudio', 2.7);
+let speedXVid = localStorage.getItem('speedXVid') ?? 2.9;
 let speedXAudio = localStorage.getItem('speedXAudio') ?? 2.9;
 
-[...document.getElementsByTagName('video')].forEach(el => el.playbackRate = speedXVid);
-[...document.getElementsByTagName('audio')].forEach(el => el.playbackRate = speedXAudio);
+function speedUp() {
+	[...document.getElementsByTagName('video')].forEach(el => el.playbackRate = speedXVid);
+	[...document.getElementsByTagName('audio')].forEach(el => el.playbackRate = speedXAudio);
+}
+speedUp();
 
+let oldURL = "";
+function checkURLchange(){
+	const currentURL = window.location.href;
+    if(currentURL != oldURL){
+        speedUp();
+        oldURL = currentURL;
+    }
+
+    setInterval(function() {
+        checkURLchange();
+    }, 4000);
+}
+checkURLchange();
+
+//window.addEventListener('hashchange', function(e){console.log('hash changed')});
+//window.addEventListener('popstate', function(e){console.log('url changed')});
+
+/*window.onhashchange = function(){
+	console.log("CCCCCCC");
+}*/
 
 /*window.browser = (function () {
   return window.msBrowser ||
